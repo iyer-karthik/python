862998515961484af4aa062ab9f0fa524aa6b1d5
Karthik Iyer <karthik2@gmx.com>
2019-02-09 12:38:10 -0500

    Create scaling.py

---
+++ snippets/scaling.py
@@ -0,0 +1,9 @@
+## Scale only continuous features and save the scaling factors.
+
+quant_features = ['casual', 'registered', 'cnt', 'temp', 'hum', 'windspeed']
+# Store scalings in a dictionary so we can convert back later
+scaled_features = {}
+for each in quant_features:
+    mean, std = data[each].mean(), data[each].std()
+    scaled_features[each] = [mean, std]
+    data.loc[:, each] = (data[each] - mean)/std
