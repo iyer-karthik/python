f219e0edffff9fc54b60a02a2f907e161228f59f
Karthik Iyer <karthik2@gmx.com>
2019-03-24 13:08:46 -0400

    Added examples and edited doc strings

--- utils/partial_dependence/modified_preprocessors.py
+++ utils/partial_dependence/modified_preprocessors.py
@@ -2,9 +2,9 @@
 # -*- author: Karthik Iyer -*-
 """
 Most scikit-learn transformers output a numpy ndarray regardless of the
-original datatype. If put in a pipeline this datatype is lost. In this script
-we aim modify the transform() method for some of these transformers so that
-they return a pandas dataframe if the fit() method is called on a dataframe. 
+original datatype. This datatype is lost if put in a pipeline. In this script
+we modify the transform() method for some of these transformers so that
+it returns a pandas dataframe if the fit() method is called on a pandas dataframe. 
 This is especially important if we wish to do some feature importance analysis
 later on. 
 
@@ -35,7 +35,7 @@ def custom_transform(obj, X):
     `transform()` method for scklearn's preprocessors/ transformers 
     is a numpy ndarray, regardless of how it was fit with.  This helper function 
     changes that behavior slightly and returns a pandas dataframe if the preprocessor/
-    transformer was fit with a dataframe, otherwise it returns
+    transformer was fit with a dataframe, otherwises it returns
     a numpy ndarray. 
     
     
@@ -54,12 +54,13 @@ def custom_transform(obj, X):
 
 
 class BinarizerDf(Binarizer):
-    """Wrapper around Binarizer
+    """
+    Wrapper around Binarizer
 
     Examples
     --------
     >>> import pandas as pd
-    >>> df = pd.DataFrame(np.random.randint(0,100,size=(10, 4)), 
+    >>> df = pd.DataFrame(np.random.randint(0, 100, size=(10, 4)), 
                           columns=list('ABCD'))
         A   B   C   D
     0  51  51  61  35
@@ -83,7 +84,25 @@ class BinarizerDf(Binarizer):
 
 
 class MaxAbsScalerDf(MaxAbsScaler):
-    """Wrapper around MaxAbsScaler"""
+    """
+    Wrapper around MaxAbsScaler
+
+    Examples
+    --------
+    >>> import pandas as pd
+    >>> df = pd.DataFrame(np.random.randint(0, 100, size=(3, 4)), 
+                          columns=list('ABCD'))
+        A   B   C   D
+    0  63  76  93  85
+    1  76   4   7  29
+    2  96  70  65  44
+    >>> scaler = MaxAbsScalerDf().fit(df) # fit does nothing.
+    >>> scaler.transform(df)
+           A         B         C         D
+    0  0.656250  1.000000  1.000000  1.000000
+    1  0.791667  0.052632  0.075269  0.341176
+    2  1.000000  0.921053  0.698925  0.517647
+    """
     
     def __init__(self, copy=True):
         super().__init__(copy=copy)
@@ -93,7 +112,25 @@ class MaxAbsScalerDf(MaxAbsScaler):
 
 
 class MinMaxScalerDf(MinMaxScaler):
-    """Wrapper around MinMaxScaler"""
+    """
+    Wrapper around MinMaxScaler
+
+    Examples
+    --------
+    >>> import pandas as pd
+    >>> df = pd.DataFrame(np.random.randint(0, 100, size=(3, 4)), 
+                          columns=list('ABCD'))
+        A   B   C   D
+    0  63  76  93  85
+    1  76   4   7  29
+    2  96  70  65  44
+    >>> scaler = MinMaxScalerDf().fit(df) # fit does nothing.
+    >>> scaler.transform(df)
+        A         B         C         D
+    0  0.000000  1.000000  1.000000  1.000000
+    1  0.393939  0.000000  0.000000  0.000000
+    2  1.000000  0.916667  0.674419  0.267857
+    """
 
     def __init__(self, feature_range=(0, 1), copy=True):
         super().__init__(copy=copy, feature_range=feature_range)
@@ -103,7 +140,25 @@ class MinMaxScalerDf(MinMaxScaler):
 
 
 class StandardScalerDf(StandardScaler):
-    """Wrapper around StandardScaler"""
+    """
+    Wrapper around StandardScaler
+
+    Examples
+    --------
+    >>> import pandas as pd
+    >>> df = pd.DataFrame(np.random.randint(0, 100, size=(3, 4)), 
+                          columns=list('ABCD'))
+        A   B   C   D
+    0  63  76  93  85
+    1  76   4   7  29
+    2  96  70  65  44
+    >>> scaler = StandardScalerDf().fit(df) # fit does nothing.
+    >>> scaler.transform(df)
+        A         B         C         D
+    0 -1.129706  0.797081  1.061028  1.366062
+    1 -0.171912 -1.410220 -1.340245 -0.999901
+    2  1.301618  0.613139  0.279218 -0.366161
+    """
 
     def __init__(self, copy=True, with_mean=True, with_std=True):
         super().__init__(copy=copy, with_mean=with_mean, with_std=with_std)
@@ -112,7 +167,28 @@ class StandardScalerDf(StandardScaler):
         return custom_transform(self, X)
 
 class SimpleImputerDf(SimpleImputer):
-    """Wrapper around SimpleImputer"""
+    """
+    Wrapper around SimpleImputer
+
+    Examples
+    -------
+    >>> import pandas as pd
+    >>> df = pd.DataFrame(np.random.randint(0, 100, size=(3, 4)), 
+                          columns=list('ABCD'))
+    >>> df['A'][0], df['B'][2] = np.nan, np.nan
+        A     B   C   D
+    0   NaN  76.0  93  85
+    1  76.0   4.0   7  29
+    2  96.0   NaN  65  44
+    >>> imp = SimpleImputerDf()
+    >>> SimpleImputerDf(copy=True, fill_value=None, missing_values=nan,
+        strategy='mean', verbose=0)
+    >>> imp.fit_transform(df)
+        A     B     C     D
+    0  86.0  76.0  93.0  85.0
+    1  76.0   4.0   7.0  29.0
+    2  96.0  40.0  65.0  44.0
+    """
 
     def __init__(self, copy=True, fill_value=None, missing_values=np.nan, 
                  strategy='mean', verbose=0):
