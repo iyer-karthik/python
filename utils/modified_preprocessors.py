4271003f59577cb313316524962b2b1a7fa6669c
unknown <karthik2@gmx.com>
2019-05-07 20:12:48 -0400

    Updated according to autopep8

--- utils/modified_preprocessors.py
+++ utils/modified_preprocessors.py
@@ -4,14 +4,14 @@
 Most scikit-learn transformers output a numpy ndarray regardless of the
 original datatype. This datatype is lost if put in a pipeline. In this script
 we modify the transform() method for some of these transformers so that
-it returns a pandas dataframe if the fit() method is called on a pandas dataframe. 
+it returns a pandas dataframe if the fit() method is called on a pandas dataframe.
 This is especially important if we wish to do some feature importance analysis
-later on. 
+later on.
 
 A manual solution would be to use sklearn's transform() method and assign
 the transformed data back into a dataframe. If we want to stick the usual
 transformers into a pipeline then we need to modify the behavior of these
-transformers, which is what this .py file does. 
+transformers, which is what this .py file does.
 
 TO DO: Add implementations for other preprocessors.
 """
@@ -27,25 +27,25 @@ from sklearn.preprocessing import StandardScaler
 
 def custom_transform(obj, X):
     """
-    Modifies scikit-learn's pre-processors/ transformers return type 
+    Modifies scikit-learn's pre-processors/ transformers return type
     for `transform()` method
-    
-    Custom implementation that modifies the return type of 
-    some of scikit-learn's preprocessors. The default return type for 
-    `transform()` method for scklearn's preprocessors/ transformers 
-    is a numpy ndarray, regardless of how it was fit with.  This helper function 
+
+    Custom implementation that modifies the return type of
+    some of scikit-learn's preprocessors. The default return type for
+    `transform()` method for scklearn's preprocessors/ transformers
+    is a numpy ndarray, regardless of how it was fit with.  This helper function
     changes that behavior slightly and returns a pandas dataframe if the preprocessor/
     transformer was fit with a dataframe, otherwises it returns
-    a numpy ndarray. 
-    
-    
+    a numpy ndarray.
+
+
     Parameters
     ----------
     obj : An instance of an object from sklearn.preprocessing or sklearn.impute
-        
+
     X : array_like
         Input data that will be transformed
-    
+
     """
     if isinstance(X, pd.core.frame.DataFrame):
         z = super(type(obj), obj).transform(X.values)
@@ -60,7 +60,7 @@ class BinarizerDf(Binarizer):
     Examples
     --------
         >>> import pandas as pd
-        >>> df = pd.DataFrame(np.random.randint(0, 100, size=(10, 4)), 
+        >>> df = pd.DataFrame(np.random.randint(0, 100, size=(10, 4)),
                           columns=list('ABCD'))
             A   B   C   D
         0  51  51  61  35
@@ -90,7 +90,7 @@ class MaxAbsScalerDf(MaxAbsScaler):
     Examples
     --------
         >>> import pandas as pd
-        >>> df = pd.DataFrame(np.random.randint(0, 100, size=(3, 4)), 
+        >>> df = pd.DataFrame(np.random.randint(0, 100, size=(3, 4)),
                             columns=list('ABCD'))
             A   B   C   D
         0  63  76  93  85
@@ -103,10 +103,10 @@ class MaxAbsScalerDf(MaxAbsScaler):
         1  0.791667  0.052632  0.075269  0.341176
         2  1.000000  0.921053  0.698925  0.517647
     """
-    
+
     def __init__(self, copy=True):
         super().__init__(copy=copy)
-    
+
     def transform(self, X):
         return custom_transform(self, X)
 
@@ -118,7 +118,7 @@ class MinMaxScalerDf(MinMaxScaler):
     Examples
     --------
         >>> import pandas as pd
-        >>> df = pd.DataFrame(np.random.randint(0, 100, size=(3, 4)), 
+        >>> df = pd.DataFrame(np.random.randint(0, 100, size=(3, 4)),
                             columns=list('ABCD'))
             A   B   C   D
         0  63  76  93  85
@@ -134,7 +134,7 @@ class MinMaxScalerDf(MinMaxScaler):
 
     def __init__(self, feature_range=(0, 1), copy=True):
         super().__init__(copy=copy, feature_range=feature_range)
-    
+
     def transform(self, X):
         return custom_transform(self, X)
 
@@ -146,7 +146,7 @@ class StandardScalerDf(StandardScaler):
     Examples
     --------
         >>> import pandas as pd
-        >>> df = pd.DataFrame(np.random.randint(0, 100, size=(3, 4)), 
+        >>> df = pd.DataFrame(np.random.randint(0, 100, size=(3, 4)),
                             columns=list('ABCD'))
             A   B   C   D
         0  63  76  93  85
@@ -162,9 +162,10 @@ class StandardScalerDf(StandardScaler):
 
     def __init__(self, copy=True, with_mean=True, with_std=True):
         super().__init__(copy=copy, with_mean=with_mean, with_std=with_std)
-    
+
     def transform(self, X):
         return custom_transform(self, X)
+
 
 class SimpleImputerDf(SimpleImputer):
     """
@@ -173,7 +174,7 @@ class SimpleImputerDf(SimpleImputer):
     Examples
     -------
         >>> import pandas as pd
-        >>> df = pd.DataFrame(np.random.randint(0, 100, size=(3, 4)), 
+        >>> df = pd.DataFrame(np.random.randint(0, 100, size=(3, 4)),
                             columns=list('ABCD'))
         >>> df['A'][0], df['B'][2] = np.nan, np.nan
             A     B   C   D
@@ -190,12 +191,12 @@ class SimpleImputerDf(SimpleImputer):
         2  96.0  40.0  65.0  44.0
     """
 
-    def __init__(self, copy=True, fill_value=None, missing_values=np.nan, 
+    def __init__(self, copy=True, fill_value=None, missing_values=np.nan,
                  strategy='mean', verbose=0):
         super().__init__(copy=copy, fill_value=fill_value,
                          missing_values=missing_values,
                          strategy=strategy,
                          verbose=verbose)
-    
+
     def transform(self, X):
         return custom_transform(self, X)
